53953b8d726dceda0d9c494c49672272fc1511e4
B Graham <139028622+Flinterpop@users.noreply.github.com>
2025-03-12 20:29:42 -0400

    Initial Commit - working and usable

---
+++ main/CommandListener.cpp
@@ -0,0 +1,49 @@
+/** CommandListener.cpp
+ *
+ * See header file for details.
+ */
+
+#include "Commandlistener.h"
+
+CommandListener::CommandListener() {
+}
+
+CommandListener::~CommandListener() {
+}
+
+void CommandListener::goRocketDown() {
+}
+
+
+void CommandListener::goRocketUp() {
+}
+
+void CommandListener::goRocketLeft() {
+}
+
+void CommandListener::goRocketRight() {
+}
+
+void CommandListener::goUp() {
+}
+	
+void CommandListener::goLeft() {
+}
+	
+void CommandListener::goRight() {
+}
+	
+void CommandListener::goDown() {
+}
+
+void CommandListener::goNowhere() {
+}
+	
+void CommandListener::doInteractWith() {
+}
+
+void CommandListener::doExit() {
+}
+
+void CommandListener::handleKeyPress(int key) {
+}
